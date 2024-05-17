4b6b4192df04d4cc2dd3a65c3ac667f1abc7c49d
LebogangMputle <lebozikalala@gmail.com>
2024-05-17 15:27:38 +0200

    Tried using the export to preview the books

--- components/BookPreview.js
+++ components/BookPreview.js
@@ -1,26 +1,41 @@
-
-
+import { authors } from "../data";
 class BookPreview extends HTMLElement {
-  constructor() {
-    super();
-    // ... component logic goes here
-    const getElement = (selector) => document.querySelector(selector);
-    const createBookPreviews = (books, container) => {
-      const fragment = document.createDocumentFragment();
-      books.forEach(({ author, id, image, title }) => {
-        const element = document.createElement("button");
-        element.classList = "preview";
-        element.dataset.preview = id;
-        element.innerHTML = `
-          <img class="preview__image" src="${image}" />
+    constructor() {
+      super();
+      this.shadowRoot = this.attachShadow({ mode: "open" });
+      this.handleClick = this.handleClick.bind(this);
+    }
+  
+    static get observedAttributes() {
+      return ["book"];
+    }
+  
+    attributeChangedCallback(name, oldValue, newValue) {
+      if (name === "book") {
+        this.render(JSON.parse(newValue));
+      }
+    }
+  
+    render(book) {
+      this.shadowRoot.innerHTML = `
+        <button class="preview" data-preview="${book.id}">
+          <img class="preview__image" src="${book.image}" alt="${book.title}" />
           <div class="preview__info">
-            <h3 class="preview__title">${title}</h3>
-            <div class="preview__author">${authors[author]}</div>
+            <h3 class="preview__title">${book.title}</h3>
+            <div class="preview__author">${book.author}</div>
           </div>
-        `;
-        fragment.appendChild(element);
-      });
-      container.appendChild(fragment);
-    };
+        </button>
+      `;
+  
+      this.shadowRoot.querySelector("button").addEventListener("click", this.handleClick);
+    }
+  
+    handleClick(event) {
+      const previewId = event.currentTarget.dataset.preview;
+      // Trigger an event to notify the application about the clicked preview
+      this.dispatchEvent(new CustomEvent("book-clicked", { detail: { previewId } }));
+    }
   }
-}
+  
+  customElements.define("book-preview", BookPreview);
+  
